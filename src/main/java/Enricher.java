ba2b3d95a8d3b8e43bbe1ddd4dd4f9285853b5ac
Krzysztof Pijewski <Krzysztof.Pijewski@gmail.com>
2016-10-25 07:50:52 +0200

    Initial commit.

---
+++ src/main/java/Enricher.java
@@ -0,0 +1,17 @@
+
+public class Enricher {
+
+	public Object enrich(Object o) {
+		return "{"+
+	"\"value\"     : \"100.50\","+
+	"\"valueType\" : \"price\","+
+	"\"date\"      : 20102016,"+
+	"\"time\"      : 103052,"+
+	"\"currency\"  : \"USD\","+
+	"\"trader\"    : \"John Smith\","+
+	"\"traderBook\": \"ABCD\","+
+	"\"system\"    : \"MarketAcessLondon\""+
+"}";
+	}
+	
+}
